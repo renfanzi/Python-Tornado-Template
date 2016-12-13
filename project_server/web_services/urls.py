0c58a70bca9b70572efb733d57ae145241b57cf8
renfanzi <3168546047@qq.com>
2016-12-13 18:13:31 +0800

    readme

--- project_server/web_services/urls.py
+++ project_server/web_services/urls.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from app.server.web_services.test_view import MainHandler
+from app.project_server.web_services.test_view import MainHandler
 
 urls = [
     (r'/aaa/', MainHandler),
--- run.py
+++ run.py
@@ -12,7 +12,7 @@ from app.comm.util.include_url_model import url_wrapper, include
 from app.comm.util.write_error import write_error
 
 application = tornado.web.Application(url_wrapper([
-    (r"", include('app.server.web_services.urls')),
+    (r"", include('app.project_server.web_services.urls')),
 ]), **settings)
 
 tornado.web.RequestHandler.write_error = write_error
@@ -25,8 +25,8 @@ if __name__ == "__main__":
     #3.
     # sockets = tornado.netutil.bind_sockets(8888)
     # tornado.process.fork_processes(5)
-    # server = HTTPServer(application)
-    # server.add_sockets(sockets)
+    # project_server = HTTPServer(application)
+    # project_server.add_sockets(sockets)
     # tornado.ioloop.IOLoop.instance().start()
 
     #2.
